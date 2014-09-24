1241155d04da7088d8dc0d2052c3a1ba4be20693
Daniel Gollahon <Daniel.Gollahon@gmail.com>
2014-09-24 10:40:45 -0700

    Unit test for species model

---
+++ mean/app/tests/species.server.model.test.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var should = require('should'),
+	mongoose = require('mongoose'),
+	Species = mongoose.model('Species')
+
+/**
+ * Globals
+ */
+var species;
+
+/**
+ * Unit tests
+ */
+describe('Species Model Unit Tests:', function() {
+	beforeEach(function(done) {
+		species = new Species({
+			id: 123,
+			name: "Red Panda"
+		});
+		done();
+	});
+
+	describe('Method Save', function() {
+		it('should be able to save without problems', function(done) {
+			return species.save(function(err) {
+				should.not.exist(err);
+				done();
+			});
+		});
+
+		it('should be able to show an error when trying to save without a species id', function(done) {
+			species.id = null;
+
+			return species.save(function(err) {
+				should.exist(err);
+				done();
+			});
+		});
+
+		it('should be able to show an error when trying to save without a species name', function(done) {
+			species.name = '';
+
+			return species.save(function(err) {
+				should.exist(err);
+				done();
+			});
+		});
+	});
+
+	afterEach(function(done) {
+		Species.remove().exec();
+		done();
+	});
+});
