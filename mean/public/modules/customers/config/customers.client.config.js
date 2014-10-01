c68410052889983e7c6c00794d80ed14ee9a8c81
jeanralphaviles <jeanralph.aviles@gmail.com>
2014-10-01 13:52:33 -0400

    Removed the Customers Dropdown

--- mean/public/modules/customers/config/customers.client.config.js
+++ mean/public/modules/customers/config/customers.client.config.js
@@ -3,9 +3,5 @@
 // Configuring the Customers module
 angular.module('customers').run(['Menus',
   function(Menus) {
-    // Set top bar menu items
-    Menus.addMenuItem('topbar', 'Customers', 'customers', 'dropdown', '/customers(/create)?');
-    Menus.addSubMenuItem('topbar', 'customers', 'List Customers', 'customers');
-    Menus.addSubMenuItem('topbar', 'customers', 'New Customer', 'customers/create');
   }
 ]);
