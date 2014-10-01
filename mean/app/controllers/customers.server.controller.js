a8599948baeeb0524d151e84f3c513581d0f5032
jeanralphaviles <jeanralph.aviles@gmail.com>
2014-10-01 13:44:10 -0400

    Adding routing logic for Customers

--- mean/app/controllers/customers.server.controller.js
+++ mean/app/controllers/customers.server.controller.js
@@ -4,39 +4,106 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-    _ = require('lodash');
+  errorHandler = require('./errors'),
+  Customer = mongoose.model('Customer'),
+  _ = require('lodash');
 
 /**
- * Create a Customer
+ * Create a customer
  */
 exports.create = function(req, res) {
+  var customer = new Customer(req.body);
+  customer.user = req.user;
 
+  customer.save(function(err) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.jsonp(customer);
+    }
+  });
 };
 
 /**
- * Show the current Customer
+ * Show the current customer
  */
 exports.read = function(req, res) {
-
+  res.jsonp(req.customer);
 };
 
 /**
- * Update a Customer
+ * Update a customer
  */
 exports.update = function(req, res) {
+  var customer = req.customer;
 
+  customer = _.extend(customer, req.body);
+
+  customer.save(function(err) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.jsonp(customer);
+    }
+  });
 };
 
 /**
- * Delete an Customer
+ * Delete an customer
  */
 exports.delete = function(req, res) {
+  var customer = req.customer;
 
+  customer.remove(function(err) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.jsonp(customer);
+    }
+  });
 };
 
 /**
  * List of Customers
  */
 exports.list = function(req, res) {
+  Customer.find().sort('-created').populate('user', 'displayName').exec(function(err, customers) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.jsonp(customers);
+    }
+  });
+};
 
+/**
+ * Customer middleware
+ */
+exports.customerByID = function(req, res, next, id) {
+  Customer.findById(id).populate('user', 'displayName').exec(function(err, customer) {
+    if (err) return next(err);
+    if (!customer) return next(new Error('Failed to load customer ' + id));
+    req.customer = customer;
+    next();
+  });
+};
+
+/**
+ * Customer authorization middleware
+ */
+exports.hasAuthorization = function(req, res, next) {
+  if (req.customer.user.id !== req.user.id) {
+    return res.status(403).send({
+      message: 'User is not authorized'
+    });
+  }
+  next();
 };
--- mean/app/routes/customers.server.routes.js
+++ mean/app/routes/customers.server.routes.js
@@ -1,6 +1,22 @@
 'use strict';
 
+/**
+ * Module dependencies.
+ */
+var users = require('../../app/controllers/users'),
+  customers = require('../../app/controllers/customers');
+
 module.exports = function(app) {
-	// Routing logic   
-	// ...
+  // Customer Routes
+  app.route('/customers')
+  .get(customers.list)
+  .post(users.requiresLogin, customers.create);
+
+  app.route('/customers/:customerId')
+    .get(customers.read)
+    .put(users.requiresLogin, customers.hasAuthorization, customers.update)
+    .delete(users.requiresLogin, customers.hasAuthorization, customers.delete);
+
+  // Finish by binding the customer middleware
+  app.param('customerId', customers.customerByID);
 };
