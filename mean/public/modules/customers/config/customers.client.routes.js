9a8e12d2fa0e001f13f3df962e98cd98a60bd77a
Daniel Gollahon <Daniel.Gollahon@gmail.com>
2014-10-01 10:47:36 -0700

    added create customer page

--- mean/public/modules/customers/config/customers.client.routes.js
+++ mean/public/modules/customers/config/customers.client.routes.js
@@ -8,6 +8,10 @@ angular.module('customers').config(['$stateProvider',
 		state('customers', {
 			url: '/customers',
 			templateUrl: 'modules/customers/views/customers.client.view.html'
+		}).
+		state('createCustomer', {
+			url: '/customers/create',
+			templateUrl: 'modules/customers/views/create-customer.client.view.html'
 		});
 	}
 ]);
