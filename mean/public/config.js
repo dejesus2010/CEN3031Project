6399bd47beaf8c5449a9313a5f237ae1c8782aaa
unknown <Daniel.Gollahon@gmail.com>
2014-10-03 18:15:21 -0400

    Added ngSanitize -- resolved errors in chrome console, should be more secure

--- mean/public/config.js
+++ mean/public/config.js
@@ -4,7 +4,7 @@
 var ApplicationConfiguration = (function() {
 	// Init module configuration options
 	var applicationModuleName = 'mean';
-	var applicationModuleVendorDependencies = ['ngResource', 'ngAnimate', 'ui.router', 'ui.bootstrap', 'ui.utils', 'ui.select'];
+	var applicationModuleVendorDependencies = ['ngResource', 'ngAnimate', 'ui.router', 'ui.bootstrap', 'ui.utils', 'ui.select', 'ngSanitize'];
 
 	// Add a new vertical module
 	var registerModule = function(moduleName, dependencies) {
