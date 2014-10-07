85f4aa02bdf8c83170255ec09907c8ee7fdbfdda
dejesus2010 <dejesus2010@gmail.com>
2014-10-07 00:04:44 -0400

    Testing the retrieval of the customer and species object of a project
    I am testing that the customer and species fields use DBRef correctly. At the current state, the customer field is populated with the customer object corresponding to the objectID when retrieving a Project document, but the species field is NOT populated. This issue may be due to the "Specie"/"Species" namespace conflict?? I have filed a bug to track this issue.

--- mean/app/tests/project.server.model.test.js
+++ mean/app/tests/project.server.model.test.js
@@ -19,19 +19,35 @@ var project;
  * Unit tests
  */
 describe('Project Model Unit Tests:', function() {
+    var customerID;
+    var speciesID;
+
+    before(function(done){
+        var customer = new Customer({id:1, name: 'University of Florida', code: 'UFL'});
+        var species = new Species({id: 3, name: 'squirrel'});
+
+        customer.save(function(err, doc){
+            customerID = doc._id;
+            species.save(function(err, doc){
+                speciesID = doc._id;
+                done();
+            });
+        });
+    });
+
 	beforeEach(function(done) {
 		project = new Project({
 			projectCode: 'ABC_010203',
 			due: Date.now(),
-			customer: new Customer({id: 1, name: 'University of Florida', code: 'UFL'}),
-			species: new Species({id: 3, name: 'squirrel'}),
+			customer: customerID,
+			species: speciesID,
 			description: 'ABC has sent us squirrel dna.',
 			user: new User({
 				firstName: 'Tim',
 				lastName: 'Tebow',
 				email: 'tim@ufl.edu',
 				password: 'heismen',
-				provider: 'local',
+				provider: 'local'
 			})
 		});
 		done();
@@ -44,6 +60,32 @@ describe('Project Model Unit Tests:', function() {
 				done();
 			});
 		});
+
+        it('project should contain a customer object equal to the Customer document with the same objectID', function(done){
+             return project.save(function(err){
+                Project.findOne({customer: customerID}).populate('customer').exec(function(err, projectDoc){
+                    Customer.findOne({_id: customerID}).exec(function(err, customerDoc){
+                        JSON.stringify(customerDoc).should.equal(JSON.stringify(projectDoc.customer));
+                        done();
+                    });
+                });
+             });
+        });
+
+        it('project should contain a species object equal to the Species document with the same objectID', function(done){
+            return project.save(function(err){
+                Project.findOne({species: speciesID}).populate('specie').exec(function(err, projectDoc){
+                    Species.findOne({_id: speciesID}).exec(function(err, speciesDoc){
+                        console.log('projectDoc');
+                        console.log(projectDoc);
+                        console.log('speciesDoc');
+                        console.log(speciesDoc);
+                        JSON.stringify(speciesDoc).should.equal(JSON.stringify(projectDoc.species));
+                        done();
+                    });
+                });
+            });
+        });
 
 		it('should be able to show an error when trying to save without a project code', function(done) {
 			project.projectCode = '';
@@ -99,6 +141,12 @@ describe('Project Model Unit Tests:', function() {
 			});
 		});
 	});
+
+    after(function(done){
+        Customer.remove().exec();
+        Species.remove().exec();
+        done();
+    });
 
 	afterEach(function(done) {
 		Project.remove().exec();
