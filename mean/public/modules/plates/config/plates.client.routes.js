1caaa37d0540ce378a95157a3a41bbafc728258a
jeanralphaviles <jeanralph.aviles@gmail.com>
2014-11-19 02:39:37 -0500

    Routing and Menu entry for ProjectList

--- mean/public/modules/plates/config/plates.client.routes.js
+++ mean/public/modules/plates/config/plates.client.routes.js
@@ -5,6 +5,10 @@ angular.module('plates').config(['$stateProvider',
 	function($stateProvider) {
 		// Plates state routing
 		$stateProvider.
+    state('listPlates', {
+      url: '/plates/listPlates',
+      templateUrl: 'modules/plates/views/list-plates.client.view.html'
+    }).
   	state('viewPlate', {
       url: '/plates/:plateId',
       templateUrl: 'modules/plates/views/plates.client.view.html'
