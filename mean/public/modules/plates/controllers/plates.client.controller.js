21cd7b7c0bd807357655ed88491c2778d77128cb
jeanralphaviles <jeanralph.aviles@gmail.com>
2014-11-19 17:09:51 -0500

    Added Tab for Unassigned/Assigned/All plates

--- mean/public/modules/plates/controllers/plates.client.controller.js
+++ mean/public/modules/plates/controllers/plates.client.controller.js
@@ -9,10 +9,22 @@ angular.module('plates').controller('PlatesController', ['$scope', '$http', '$st
     $scope.grayOut = false;
     $scope.plateList = [];
     $scope.selectedPlate = null;
+    $scope.error = '';
+    // Tab flags
+    $scope.unassignedTab = false;
+    $scope.assignedTab = false;
+    $scope.allTab = false;
 
-    $scope.init = function() {
+    $scope.init = function(tab) {
       $scope.closeDialog();
-      $http.get('/plates').success(function(response) {
+      var url = '/plates';
+      switch (tab) {
+        case 'unassinged': $scope.unassignedTab = true; url = url.concat('/unassigned'); break;
+        case 'assigned':   $scope.assignedTab = true; url = url.concat('/assigned'); break;
+        case 'all':        $scope.allTab = true; break;
+        default:           $scope.unassignedTab = true; url = url.concat('/unassigned'); break;
+      }
+      $http.get(url).success(function(response) {
         $scope.plateList = response;
         $scope.initReactGrid();
       }).error(function(err) {
@@ -21,7 +33,17 @@ angular.module('plates').controller('PlatesController', ['$scope', '$http', '$st
       });
     };
 
+    $scope.switchTab = function(tab) {
+      $scope.unassignedTab = false;
+      $scope.assignedTab = false;
+      $scope.allTab = false;
+
+      $scope.init(tab);
+    };
+
     $scope.initReactGrid = function() {
+      // We set the gridReady to false here to force the DOM to update
+      $scope.gridReady = false;
       $scope.grid.data = JSON.parse(JSON.stringify($scope.plateList));
       for (var plate = 0; plate < $scope.grid.data.length; ++plate) {
         // Make dates human readable
@@ -33,6 +55,9 @@ angular.module('plates').controller('PlatesController', ['$scope', '$http', '$st
     $scope.addPlate = function() {
       if ($scope.selectedPlate === null) {
         $scope.error = 'No plate is selected';
+        $scope.errorDialog();
+      } else if ($scope.selectedPlate.isAssigned) {
+        $scope.error = 'Plate is already assigned';
         $scope.errorDialog();
       } else {
         $http.post('/plates/assignPlate', $scope.selectedPlate).success(function(response) {
@@ -57,7 +82,11 @@ angular.module('plates').controller('PlatesController', ['$scope', '$http', '$st
     };
 
     $scope.errorDialog = function() {
+      // Save error
+      var error = $scope.error;
+      // Makes sure we'll never have two confirmation dialogs
       $scope.closeDialog();
+      $scope.error = error;
       ngDialog.open({
         template: 'errorDialog',
         className: 'ngdialog-theme-default',
@@ -69,6 +98,7 @@ angular.module('plates').controller('PlatesController', ['$scope', '$http', '$st
 
     $scope.closeDialog = function() {
       ngDialog.closeAll();
+      $scope.error = '';
       $scope.grayOut = false;
     };
 
