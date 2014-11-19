b8a7765888935c0f3336d7f4f568c7ccf2e7b751
jeanralphaviles <jeanralph.aviles@gmail.com>
2014-11-19 12:36:25 -0500

    List Plates page

--- mean/public/modules/plates/controllers/plates.client.controller.js
+++ mean/public/modules/plates/controllers/plates.client.controller.js
@@ -1,81 +1,89 @@
 'use strict';
 
-angular.module('plates').controller('PlatesController', ['$scope', '$stateParams', '$location', '$window', 'Authentication', 'Plates',
-  function($scope, $stateParams, $location, $window, Authentication, Plates) {
+angular.module('plates').controller('PlatesController', ['$scope', '$http', '$stateParams', '$location', '$window', 'ngDialog', 'Authentication', 'Plates',
+  function($scope, $http, $stateParams, $location, $window, ngDialog, Authentication, Plates) {
     $scope.authentication = Authentication;
 
-    $scope.remove = function(plate) {
-      if (plate) {
-        plate.$remove();
+    $scope.authentication = Authentication;
+    $scope.gridReady = false;
+    $scope.grayOut = false;
+    $scope.plateList = [];
+    $scope.selectedPlate = null;
 
-        for (var i in $scope.plates) {
-          if ($scope.plates[i] === plate) {
-            $scope.plates.splice(i, 1);
-          }
-        }
+    $scope.init = function() {
+      $scope.closeDialog();
+      $http.get('/plates').success(function(response) {
+        $scope.plateList = response;
+        $scope.initReactGrid();
+      }).error(function(err) {
+        // Figure out what to do if listPlates fails
+      });
+    };
+
+    $scope.initReactGrid = function() {
+      $scope.grid.data = JSON.parse(JSON.stringify($scope.plateList));
+      for (var plate = 0; plate < $scope.grid.data.length; ++plate) {
+        // Make dates human readable
+        $scope.grid.data[plate].project.due = new Date(Date.parse($scope.grid.data[plate].project.due)).toISOString().slice(0,10);
+      }
+      $scope.gridReady = true;
+    };
+
+    $scope.addPlate = function() {
+      if ($scope.selectedPlate === null) {
+        $scope.closeDialog();
       } else {
-        $scope.plate.$remove(function() {
-          $location.path('plates');
+        $http.post('/plates/assignPlate', $scope.selectedPlate).success(function(response) {
+          // Reload ngGrid
+          $scope.init();
+        }).error(function(err) {
+          // Figure out what to do
+          $scope.closeDialog();
         });
       }
     };
 
-    $scope.update = function() {
-      var plate = $scope.plate;
-
-      plate.$update(function() {
-        $location.path('plates/' + plate._id);
-      }, function(errorResponse) {
-        $scope.error = errorResponse.data.message;
+    $scope.confirmAdd = function() {
+      ngDialog.open({
+        template: 'addConfirmDialog',
+        className: 'ngdialog-theme-default',
+        scope: $scope,
+        showClose: false
       });
+      $scope.grayOut = true;
     };
 
-    $scope.findOne = function() {
-      $scope.plate = Plates.get({
-        plateId: $stateParams.plateId
-      }, function(err, doc) {
-        var plateStage = $scope.plate.stage;
-        $scope.steps[plateStage - 1].active = true;
-        // Disable the steps that haven't been reached yet
-        for (var i = plateStage - 1; i < $scope.steps.length; ++i) {
-          $scope.steps[i].disabled = true;
-        }
-      });
+    $scope.closeDialog = function() {
+      ngDialog.closeAll();
+      $scope.grayOut = false;
     };
 
-    $scope.init = function() {
-      $scope.findOne();
+    $scope.grid = {
+      data: [],
+      rowClick: function(plate) {
+        console.log($scope.authentication);
+        $scope.selectedPlate = plate;
+        $scope.confirmAdd();
+      },
+      columnDefs: [{
+        field: 'plateCode',
+        displayName: 'Plate Code',
+        width: '7%'
+      }, {
+        field: 'project.organism.name',
+        displayName: 'Organism',
+      }, {
+        field: 'project.customer.name',
+        displayName: 'Customer'
+      }, {
+        field: 'project.due',
+        displayName: 'Due Date',
+        width: '6%'
+      }, {
+        field: 'assignee.displayName',
+        displayName: 'Assignee',
+        width: '7%'
+      }]
     };
-
-    $scope.steps = [{
-        title: '1'
-      }, {
-        title: '2'
-      }, {
-        title: '3'
-      }, {
-        title: '4'
-      }, {
-        title: '5'
-      }, {
-        title: '6'
-      }, {
-        title: '7'
-      }, {
-        title: '8'
-      }, {
-        title: '9'
-      }, {
-        title: '10'
-      }, {
-        title: '11'
-      }, {
-        title: '12'
-      }, {
-        title: '13'
-      }, {
-        title: '14'
-      }
-    ];
   }
 ]);
--- mean/public/modules/plates/tests/plates.client.controller.test.js
+++ mean/public/modules/plates/tests/plates.client.controller.test.js
@@ -55,6 +55,65 @@
 			});
 		}));
 
+		it('$scope.init() should create an HTTP request for the list of plates and set $scope.plateList', inject(function() {
+			var sampleData = [{'__v':0,'_id':'546cc49feedb31ca75693aca','assignee':{'displayName':'Jean-Ralph Aviles','_id':'546aac97e6d1b14748520264'},'isAssigned':true,'plateCode':'UFL_010101_P2','project':{'__v':6,'_id':'546aacc8e6d1b14748520268','created':'2014-11-18T02:19:52.149Z','customer':{'_id':'546aacb7e6d1b14748520265','__v':0,'email':'ufl@ufl.edu','projects':[],'code':'UFL','id':1,'name':'University of Florida'},'description':'asdfad','due':'2014-11-20T05:00:00.000Z','lastEdited':'2014-11-19T04:10:55.239Z','lastEditor':'546aac97e6d1b14748520264','logs':[],'projectCode':'UFL_010101','projectStatus':false,'sequencingMethod':'','user':'546aac97e6d1b14748520264'},'samples':[],'stage':1}];
+			$httpBackend.expectGET('/plates').respond(sampleData);
+			scope.init();
+			$httpBackend.flush();
+
+			expect(scope.plateList).toEqual(sampleData);
+		}));
+
+		it('$scope.initReactGrid() should format $scope.grid.data\'s dates and set $scope.gridReady to true', inject(function() {
+			scope.plateList = [{'__v':0,'_id':'546cc49feedb31ca75693aca','assignee':{'displayName':'Jean-Ralph Aviles','_id':'546aac97e6d1b14748520264'},'isAssigned':true,'plateCode':'UFL_010101_P2','project':{'__v':6,'_id':'546aacc8e6d1b14748520268','created':'2014-11-18T02:19:52.149Z','customer':{'_id':'546aacb7e6d1b14748520265','__v':0,'email':'ufl@ufl.edu','projects':[],'code':'UFL','id':1,'name':'University of Florida'},'description':'asdfad','due':'2014-11-20T05:00:00.000Z','lastEdited':'2014-11-19T04:10:55.239Z','lastEditor':'546aac97e6d1b14748520264','logs':[],'projectCode':'UFL_010101','projectStatus':false,'sequencingMethod':'','user':'546aac97e6d1b14748520264'},'samples':[],'stage':1}];
+			scope.grid = {data: null};
+			scope.initReactGrid();
+
+			expect(scope.grid.data[0].project.due).toBe('2014-11-20');
+			expect(scope.gridReady).toBeTruthy();
+		}));
+
+		it('$scope.addPlate() should send the correct HTTP request and reinit the ngGrid', inject(function() {
+			scope.selectedPlate = {'__v':0,'_id':'546cc49feedb31ca75693aca','assignee':{'displayName':'Jean-Ralph Aviles','_id':'546aac97e6d1b14748520264'},'isAssigned':true,'plateCode':'UFL_010101_P2','project':{'__v':6,'_id':'546aacc8e6d1b14748520268','created':'2014-11-18T02:19:52.149Z','customer':{'_id':'546aacb7e6d1b14748520265','__v':0,'email':'ufl@ufl.edu','projects':[],'code':'UFL','id':1,'name':'University of Florida'},'description':'asdfad','due':'2014-11-20T05:00:00.000Z','lastEdited':'2014-11-19T04:10:55.239Z','lastEditor':'546aac97e6d1b14748520264','logs':[],'projectCode':'UFL_010101','projectStatus':false,'sequencingMethod':'','user':'546aac97e6d1b14748520264'},'samples':[],'stage':1};
+
+			$httpBackend.expectPOST('/plates/assignPlate').respond(200);
+			$httpBackend.expectGET('/plates').respond([scope.selectedPlate]);
+			scope.addPlate();
+			$httpBackend.flush();
+		}));
+
+		it('$scope.addPlate() should not send an HTTP request if a plate hasn\'t been selected and close any dialogs that may be open', inject(function() {
+			scope.selectedPlate = null;
+			scope.grayOut = true;
+
+			scope.addPlate();
+			expect(scope.grayOut).toBeFalsy();
+		}));
+
+		it('$scope.addPlate() should close the ngDialog if an HTTP error is returned', inject(function() {
+			scope.selectedPlate = {};
+			scope.grayOut = true;
+
+			$httpBackend.expectPOST('/plates/assignPlate').respond(400);
+			scope.addPlate();
+			$httpBackend.flush();
+
+			expect(scope.grayOut).toBeFalsy();
+		}));
+
+		it('$scope.confirmAdd should open the ngDialog', inject(function() {
+			scope.grayOut = false;
+			scope.confirmAdd();
+			expect(scope.grayOut).toBeTruthy();
+		}));
+
+		it('$scope.closeDialog should close the ngDialog', inject(function() {
+			scope.grayOut = true;
+			scope.closeDialog();
+			expect(scope.grayOut).toBeFalsy();
+		}));
+
+/*
 		it('$scope.findOne() should create an array with one plate object fetched from XHR using a plateId URL parameter', inject(function(Plates) {
 			// Define a sample plate object
 			var samplePlate = new Plates({
@@ -99,5 +158,6 @@
 			// Test URL location to new object
 			expect($location.path()).toBe('/plates/' + samplePlatePutData._id);
 		}));
+*/
 	});
 }());
