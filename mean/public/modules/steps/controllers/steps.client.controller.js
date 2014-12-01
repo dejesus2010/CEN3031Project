3658db5e1a9abf54bc081cf91d40d0e6df977c83
Joseph Liccini <joseph.liccini@ufl.edu>
2014-12-01 14:24:33 -0500

    Update worklist upon incrementation.

--- mean/public/modules/steps/controllers/steps.client.controller.js
+++ mean/public/modules/steps/controllers/steps.client.controller.js
@@ -6,16 +6,14 @@ angular.module('steps').controller('StepsController', ['$scope', '$stateParams',
 		// ...
 		$scope.plates = JSON.parse($stateParams.samples);
 
-		// used for debugging purposes. Allowed me to see if the selected plates were sent to the page
-		/*for(var i = 0; i < $scope.plates.length; i++){
-			console.log($scope.plates[i].plateCode);
-		}*/
-
 		$scope.incrementPlates = function() {
+			var emission = function() {
+				$scope.$emit('workListUpdated');
+			};
 			for (var i in $scope.plates) {
 				var plate = $scope.plates[i];
 				$http.post('plates/increment/' + plate._id, plate);
-				$http.post('/plates/unassignPlate', plate);
+				$http.post('/plates/unassignPlate', plate).success(emission);
 			}
 		};
 
