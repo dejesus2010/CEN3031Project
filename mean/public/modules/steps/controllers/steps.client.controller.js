4055b440bab4fb41448f7f042a8666ccd92b8ca5
Joseph Liccini <joseph.liccini@ufl.edu>
2014-12-03 21:19:37 -0500

    When 'Confirming' a step, it not only increments, but it also $emit's to for 'workListUpdated'.
    Also, redirects to the worklist after pressing.

--- mean/public/modules/steps/controllers/steps.client.controller.js
+++ mean/public/modules/steps/controllers/steps.client.controller.js
@@ -6,27 +6,40 @@ angular.module('steps').controller('StepsController', ['$scope', '$stateParams',
 		// ...
 		$scope.plates = JSON.parse($stateParams.samples);
 		$scope.removeFromWorklist = true;
+		$scope.count = 0;
 
 		$scope.incrementPlates = function() {
-			async.each($scope.plates, $scope.incrementPlate, function(err) { // jshint ignore:line
-				if (err) {
-					console.log(err);	
-				}
-			
-			}); 
+			var incCount = function() {
+				$scope.count++;
+			};
+			for(var i in $scope.plates) {
+				var plate = $scope.plates[i];
+				$scope.incrementPlate(plate, incCount);
+			}
+			$scope.waitFor($scope.count, $scope.plates.length, $scope.returnToWorklist);
 		};
 
-		$scope.incrementPlate = function(plate) {
+		$scope.waitFor = function(testValue, expectedValue, callback) {
+		    // Check if condition met. If not, re-check later (msec).
+		    // Found on Stack Overflow, very useful.
+		    if (testValue !== expectedValue) {
+			    setTimeout($scope.waitFor(testValue, expectedValue, callback), 5000);
+		    }
+		    callback();
+		};
+
+		$scope.incrementPlate = function(plate, callback) {
 			var emission = function() {
 				$scope.$emit('workListUpdated');
 			};
 			$http.post('plates/increment/', plate);
 			if ($scope.removeFromWorklist) {
-				$http.post('plates/unassignPlate', plate).then(emission);
+				$http.post('plates/unassignPlate', plate).then(callback());
 			}
 		};
 
 		$scope.returnToWorklist = function() {
+			$scope.$emit('workListUpdated');
 			$location.path('worklist');
 		};
 
