9b2b31fd2e35fb5b1ac7bf753ee566328fa9430e
Joseph Liccini <joseph.liccini@ufl.edu>
2014-12-04 23:29:03 -0500

    Added support for all steps in the client routes for the steps module.
    Typed up the end-repair html view.
    
    Still not sure how to do substeps

--- mean/public/modules/steps/config/steps.client.routes.js
+++ mean/public/modules/steps/config/steps.client.routes.js
@@ -20,6 +20,46 @@ angular.module('steps').config(['$stateProvider',
 		state('shearing', {
 			url: '/steps/shearing/:samples',
 			templateUrl: 'modules/steps/views/shearing.client.view.html'
+		}).
+		state('end-repair', {
+			url: '/steps/end-repair/:samples',
+			templateUrl: 'modules/steps/views/end-repair.client.view.html'
+		}).
+		state('adenylation', {
+			url: '/steps/adenylation/:samples',
+			templateUrl: 'modules/steps/views/adenylation.client.view.html'
+		}).
+		state('ligation', {
+			url: '/steps/ligation/:samples',
+			templateUrl: 'modules/steps/views/ligation.client.view.html'
+		}).
+		state('size-selection', {
+			url: '/steps/size-selection/:samples',
+			templateUrl: 'modules/steps/views/size-selection.client.view.html'
+		}).
+		state('pcr-enrichment', {
+			url: '/steps/pcr-enrichment/:samples',
+			templateUrl: 'modules/steps/views/pcr-enrichment.client.view.html'
+		}).
+		state('pcr-cleanup', {
+			url: '/steps/pcr-cleanup/:samples',
+			templateUrl: 'modules/steps/views/pcr-cleanup.client.view.html'
+		}).
+		state('library-quantification', {
+			url: '/steps/library-quantification/:samples',
+			templateUrl: 'modules/steps/views/library-quantification.client.view.html'
+		}).
+		state('pooling', {
+			url: '/steps/pooling/:samples',
+			templateUrl: 'modules/steps/views/pooling.client.view.html'
+		}).
+		state('hybridization', {
+			url: '/steps/hybridization/:samples',
+			templateUrl: 'modules/steps/views/hybridization.client.view.html'
+		}).
+		state('sequencing', {
+			url: '/steps/sequencing/:samples',
+			templateUrl: 'modules/steps/views/sequencing.client.view.html'
 		});
 	}
 ]);
