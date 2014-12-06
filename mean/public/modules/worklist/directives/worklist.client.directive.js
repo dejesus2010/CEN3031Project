0cfd5332e0b6debb70feccaa20043a18262db129
dejesus2010 <dejesus2010@gmail.com>
2014-12-06 18:30:04 -0500

    Fixed select all button to select all plates accordingly

--- mean/public/modules/worklist/directives/worklist.client.directive.js
+++ mean/public/modules/worklist/directives/worklist.client.directive.js
@@ -5,27 +5,42 @@ angular.module('worklist').directive('myWorklistSelected', [
 			return{
 				restrict: 'A',
 				link: function postLink(scope, element, attrs){
-					// false == have not selected all plates
+					// false == have not selected all plates (initialization), or deselect all plates
 					// true == selected all plates
 					scope.selectedAllPlates = false;
 					// creating an empty array which will hold the plate objects that are selected in a stage
 					scope.selectedPlates = [];
 
 					scope.selectAll = function(plates){
-						scope.selectedAllPlates = !scope.selectedAllPlates;
-						scope.selectedPlates = [];
-						// Can't simply set scope.selectedPlates = plates, because the next time we unselect
-						// a plate i.e removing the plate from scope.selectedPlates, it's as if we are removing
-						// from the plate array that was passed as an argument which references stagePlates.
-						// Modifying stagePlates will remove the plate's row from the table, because of two way data
-						// binding
-						if(scope.selectedAllPlates){
-							plates.forEach(function(plate){
-								scope.selectedPlates.push(plate);
-							});
+						if(scope.selectedPlates.length === plates.length){ // all plates are selected already, so deselect all
+							scope.selectedAllPlates = false;
+							scope.selectedPlates = [];
+						}
+						else if(scope.selectedPlates.length === 0){ // no plates are selected, so select all
+							scope.selectedAllPlates = true;
+							addAllPlatesToArray(plates, scope.selectedPlates);
+						}
+						else{ // some plates are selected, so do the opposite of the previous select all state
+							scope.selectedAllPlates = !scope.selectedAllPlates;
+							scope.selectedPlates = [];
+							// Can't simply set scope.selectedPlates = plates, because the next time we unselect
+							// a plate i.e removing the plate from scope.selectedPlates, it's as if we are removing
+							// from the plate array that was passed as an argument which references stagePlates.
+							// Modifying stagePlates will remove the plate's row from the table, because of two way data
+							// binding
+							if(scope.selectedAllPlates){
+								addAllPlatesToArray(plates, scope.selectedPlates);
+							}
+
 						}
 
 						scope.$broadcast('selectedAll', scope.selectedAllPlates);
+					};
+
+					var addAllPlatesToArray = function(plates, array){
+						plates.forEach(function(plate){
+							array.push(plate);
+						});
 					};
 				}
 			};
